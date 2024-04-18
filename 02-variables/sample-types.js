5f2f857c3ac9cd0e8c0be87734faf731afbe6dc8
Vuong Gia Hy <hyvuonggia@gmail.com>
2024-04-18 16:36:33 +0700

    initial commit

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.6;
+var firstName = 'Gia Hy';
+var lastName = 'Vuong';
+console.log(found);
+console.log("The grade is ".concat(grade));
+console.log("Hi ".concat(firstName, " ").concat(lastName));
